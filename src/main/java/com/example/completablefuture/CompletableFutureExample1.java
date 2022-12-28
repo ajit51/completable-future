03f30f537b8c303f4d874e91ef7ed6ddb8ffca78
ajit51 <ajitk433@gmail.com>
2022-12-28 13:34:17 +0530

    CompletableFutureExample1

---
+++ src/main/java/com/example/completablefuture/CompletableFutureExample1.java
@@ -0,0 +1,20 @@
+package com.example.completablefuture;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class CompletableFutureExample1 {
+    private static int getNumber(int a) {
+        return a * a;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(5,9,14);
+
+        list.stream()
+                .map(num->CompletableFuture.supplyAsync(()->getNumber(num)))
+                .map(CompletableFuture->CompletableFuture.thenApply(n -> n*n))
+                .map(t->t.join()).forEach(s->System.out.println(s));
+    }
+}
